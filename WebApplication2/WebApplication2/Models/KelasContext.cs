7182b5a0c4b69a1c14f6eabefc255927e61f6ab2
franklindh <101926929+franklindh@users.noreply.github.com>
2022-03-20 18:45:36 +0700

    Add files via upload

---
+++ WebApplication2/WebApplication2/Models/KelasContext.cs
@@ -0,0 +1,87 @@
+﻿using MySql.Data.MySqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebAPI.Models
+{
+    public class KelasContext:DbContext
+    {
+        private object id;
+        private object sub;
+
+        public KelasContext(DbContextOptions<KelasContext> options) : base(options)
+        {
+        }
+        public string ConnectionString { get; set; }
+
+        //public KelasContext(string connectionString)
+        //{
+        //    this.ConnectionString = connectionString;
+        //}
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection("Server = localhost; Database = sibaru; Uid = root; Pwd = ");
+        }
+
+        public List<KelasItem> GetAllSiswa()
+        {
+            List<KelasItem> list = new List<KelasItem>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM kelas2 WHERE id_kelas = @id and sub = @sub", conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@sub", sub);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new KelasItem()
+                        {
+                            id = reader.GetInt32("id_kelas"),
+                            kelas = reader.GetString("kelas"),
+                            jurusan = reader.GetString("jurusan"),
+                            sub = reader.GetInt32("sub")
+                           
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+        public List<KelasItem> GetSiswa(string id)
+        {
+            List<KelasItem> list = new List<KelasItem>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM kelas2 WHERE id_kelas = @id and sub = @sub", conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new KelasItem()
+                        {
+                            id = reader.GetInt32("id_kelas"),
+                            kelas = reader.GetString("kelas"),
+                            jurusan = reader.GetString("jurusan"),
+                            sub = reader.GetInt32("sub")
+                            
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/KelasItem.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.Models;
+
+namespace WebApplication2.Models
+{
+    public class KelasItem
+    {
+        private KelasContext context;
+        public int id { get; set; }
+        public string kelas { get; set; }
+        public string jurusan { get; set; }
+        public int sub { get; set; }
+    }
+}
